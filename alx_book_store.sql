2eb695cf8855fe717e8bb7f598eeed6a5f3707de
MinaElabd3 <minaelabd3@gmail.com>
2024-07-28 13:45:06 +0300

    Update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
--- Create database
-CREATE DATABASE alx_book_store;
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the database
 USE alx_book_store;
@@ -14,9 +14,9 @@ CREATE TABLE Authors (
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
-    author_id INT,
-    price DOUBLE,
-    publication_date DATE,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE NOT NULL,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
@@ -25,23 +25,24 @@ CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address TEXT
+    address TEXT NOT NULL
 );
 
 -- Create Orders table
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 -- Create Order_Details table
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity DOUBLE,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
